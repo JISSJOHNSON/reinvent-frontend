8db5708c810437c59a1ced90ae0aa4ccde0e2939
Ann Philip <annphilomina246@gmail.com>
2018-07-15 12:55:46 +0530

    Added basic routing and app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profile', component: ProfileComponent},
+  {path:'', component: HomeComponent},
+  {path:'**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
